a9e7e1c9d61a5653535a1ed3e3432b8bf7946875
danielmarmol159 <247783302+ldmmoreno2003-bot@users.noreply.github.com>
2026-02-03 09:58:13 -0500

    programa1

---
+++ mai4.py
@@ -0,0 +1,10 @@
+def numero ():
+        
+
+    numero = int(input("ingrese un numero"))    
+    if numero > 2 :
+        print(f"el numero que usted ingreso es {numero}")
+    else:
+        print("su numero es menor o igual a 2") 
+
+numero()
